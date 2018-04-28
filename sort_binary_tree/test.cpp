d6a5f91b3c251aedf015db1a3877d691824bc6bc
VeeBo <1134267225@qq.com>
2018-04-28 09:59:45 +0800

    Update test.cpp

--- sort_binary_tree/test.cpp
+++ sort_binary_tree/test.cpp
@@ -7,19 +7,28 @@ using namespace std;
 int main()
 {
 	Btree bt;
-	int ar[] = {4,5,3,2,7,5};
+	int ar[] = {8,10,6,7,5,3,15,9,12};
+//	int ar[] = {8,10,6};
 	for(int i=0; i<sizeof(ar)/sizeof(int); i++){
 		bt.Insert(ar[i]);
 	}
 	bt.Display();
+	bt.Delete(6);
+	bt.Display();
 	bt.Delete(5);
 	bt.Display();
-	bt.Delete(4);
-	bt.Display();
-	bt.Delete(1);
+	bt.Delete(3);
 	bt.Display();
 
+	bt.Delete(9);
+	bt.Display();
+	bt.Delete(15);
+	bt.Display();
 
+	bt.Delete(8);
+	bt.Display();
+	bt.Delete(18);
+	bt.Display();
 
 	return 0;
 }
